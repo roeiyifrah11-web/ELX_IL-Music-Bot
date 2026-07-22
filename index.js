8a32b7e14772eec9e6f1addb4edea303217c641e
roeiyifrah11-web <roeiyifrah11@gmail.com>
2026-07-22 19:53:17 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const {
+  Client,
+  GatewayIntentBits
+} = require("discord.js");
+
+
+const client = new Client({
+
+  intents: [
+
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+
+  ]
+
+});
+
+
+client.once("ready", () => {
+
+  console.log(
+    `🎵 הבוט מחובר בתור ${client.user.tag}`
+  );
+
+});
+
+
+client.login(process.env.TOKEN);
